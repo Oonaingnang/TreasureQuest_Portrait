80ed124726e0fa06658048447b5df0c066daeab5
Oonaingnang <naingnang059@gmail.com>
2025-08-12 02:53:13 +0630

    Add Player.gd

---
+++ assets/scenes/scripts/Player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 200
+var jump_velocity = -500
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	move_and_slide()
